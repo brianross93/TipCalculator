2239fb39daf86923eb8810224bb7edcb14e325e9
Brian Ross <brian.ross@students.makeschool.com>
2020-09-30 01:36:26 -0400

    Added working total per person

--- tipcalcjs.js
+++ tipcalcjs.js
@@ -5,26 +5,31 @@
 const display = document.querySelector('#display')
 const billInput = document.querySelector('#bill')
 const tipInput = document.querySelector('#tip')
+const peopleInput = document.querySelector('#people')
 const tipPaid = document.querySelector('#tipAmount')
 const submitButton = document.querySelector('#button')
 
 
 
+
     billInput.addEventListener('input', calculateTip)
     tipInput.addEventListener('input', calculateTip)
+    peopleInput.addEventListener('input', calculateTip)
 
 
 
 function calculateTip() {
-    
+    const peopleValue = parseFloat(peopleInput.value)
     const billValue = parseFloat(billInput.value)
     const tipValue = parseFloat(tipInput.value)
 
-    const tipAmount = (billValue *tipValue) / 100;
-    const billAmount = parseFloat(tipAmount) + parseFloat(billInput.value);
+    
+    const tipAmount = billValue *tipValue / 100;
+    const billAmount = (tipAmount + billValue) /peopleValue;
 
     tipPaid.innerHTML = tipAmount.toFixed(2)
-    display.innerHTML = billAmount
+    display.innerHTML = billAmount.toFixed(2)
+    
 
   
 
